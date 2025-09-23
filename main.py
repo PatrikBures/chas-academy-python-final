379a1f6fdb06b6f97a1e7ad0f3552215c8824c1a
Patrik Bures <212015008+PatrikBures@users.noreply.github.com>
2025-09-23 16:27:35 +0200

    simpe user interface

---
+++ main.py
@@ -0,0 +1,126 @@
+from simple_term_menu import TerminalMenu
+import sys
+
+
+def select_int_range(title, min, max):
+    while True:
+        num = ""
+        try:
+            num = input(title)
+        except KeyboardInterrupt:
+            return -1
+
+        try:
+            num = int(num)
+        except KeyboardInterrupt:
+            return -1
+        except ValueError:
+            print("Not a number!")
+            continue
+        if min <= num <= max:
+            return num
+        else:
+            print(f"Number not in range ({min} - {max})")
+
+def select_action(actions, title = ""):
+    options = []
+    for opt in actions.keys():
+        options.append(opt)
+
+    terminal_menu = TerminalMenu(options, title=title)
+
+    while True:
+        menu_entry_index = terminal_menu.show()
+
+        if type(menu_entry_index) == int:
+            selected_option = options[menu_entry_index]
+            action = actions[selected_option]
+
+            if action:
+                exit_loop = action()
+
+                if exit_loop:
+                    return
+        else: 
+            return True
+
+
+def start_monitoring():
+    print("start monitoring")
+def list_active_monitor():
+    print("list")
+def create_alarm():
+    alarms = {
+        "cpu": 0,
+        "ram": 0,
+        "disk": 0
+    }
+    def cpu():
+        print(f"Current CPU alarm at {alarms["cpu"]}%")
+        new_alarm = select_int_range(f"New CPU alarm (0-100)%: ", 0, 100)
+
+        if new_alarm == -1:
+            return True
+
+        alarms["cpu"] = new_alarm
+
+    def ram():
+        new_alarm = select_int_range(f"New Ram alarm (0-100)%: ", 0, 100) 
+
+        if new_alarm == -1:
+            return True
+
+        alarms["ram"] = new_alarm
+
+    def disk():
+        new_alarm = select_int_range("New Disk alarm(0-100)%: ", 0, 100)
+
+        if new_alarm == -1:
+            return True
+
+        alarms["disk"] = new_alarm
+
+    def back():
+        return True
+
+    actions = {
+        "CPU usage": cpu,
+        "Ram usage": ram,
+        "Disk usage": disk,
+        "Back": back
+
+    }
+
+    select_action(actions, "Select alarm to configure")
+
+
+def show_alarm():
+    print("show alarm")
+def start_monitoring_mode():
+    print("start mon mode")
+def remove_alarm():
+    print("remove alarm")
+def _exit():
+    sys.exit()
+
+def main():
+    actions = {
+        "start monitoring": start_monitoring,
+        "List active monitor": list_active_monitor,
+        "Create alarm": create_alarm,
+        "Show alarm": show_alarm,
+        "Start monitoring mode": start_monitoring_mode,
+        "Remove alarm": remove_alarm,
+        "Exit": _exit
+    }
+
+
+    while True:
+        exit_loop = select_action(actions)
+
+        if exit_loop:
+            break
+
+
+if __name__ == "__main__":
+    main()
