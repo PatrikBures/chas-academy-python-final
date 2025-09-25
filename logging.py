b7146ae1a3ab460f5c874ad1d95af82e140cf368
Patrik Bures <212015008+PatrikBures@users.noreply.github.com>
2025-09-25 14:10:52 +0200

    logging log function

---
+++ logging.py
@@ -0,0 +1,9 @@
+from datetime import datetime
+from pathlib import Path
+
+log_path=Path("log.csv")
+
+def log(string_to_log):
+    with open(log_path, "a") as file:
+        dt = datetime.now()
+        file.write(f"\n{dt.year}-{dt.month}-{dt.day}\t{dt.hour}:{dt.minute}:{dt.second}\t{string_to_log}")
