69fa6c23c28820237a2ed0ab51e420e404b5abec
Patrik Bures <212015008+PatrikBures@users.noreply.github.com>
2025-09-25 14:10:59 +0200

    logging create log file

--- logging.py
+++ logging.py
@@ -7,3 +7,10 @@ def log(string_to_log):
     with open(log_path, "a") as file:
         dt = datetime.now()
         file.write(f"\n{dt.year}-{dt.month}-{dt.day}\t{dt.hour}:{dt.minute}:{dt.second}\t{string_to_log}")
+
+def create_log_file():
+    if log_path.exists():
+        return
+
+    with open(log_path, "w") as file:
+        file.write("Date\tTime\tEntry")
