ee21bd8e17240f23194e1b65cece91a0bde1e066
Patrik Bures <patrikbures5@gmail.com>
2025-09-25 17:23:26 +0200

    log is comma seperated

--- custom_logging.py
+++ custom_logging.py
@@ -6,11 +6,11 @@ log_path=Path("log.csv")
 def log(string_to_log):
     with open(log_path, "a") as file:
         dt = datetime.now()
-        file.write(f"\n{dt.year}-{dt.month}-{dt.day}\t{dt.hour}:{dt.minute}:{dt.second}\t{string_to_log}")
+        file.write(f"\n{dt.year}-{dt.month}-{dt.day},{dt.hour}:{dt.minute}:{dt.second},{string_to_log}")
 
 def create_log_file():
     if log_path.exists():
         return
 
     with open(log_path, "w") as file:
-        file.write("Date\tTime\tEntry")
+        file.write("Date,Time,Entry")
